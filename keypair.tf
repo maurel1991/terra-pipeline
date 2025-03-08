9b1423664bf559d83943c0db7a3d8760b90cd9c4
olaoye <olaoyekevin2020@gmail.com>
2025-03-07 22:17:45 -0500

    project

--- keypair.tf
+++ keypair.tf
@@ -3,10 +3,10 @@ resource "tls_private_key" "tls" {
   rsa_bits  = 2048
 }
 resource "aws_key_pair" "key" {
-  key_name   = "utc-key"
+  key_name   = "utcK"
   public_key = tls_private_key.tls.public_key_openssh
 }
 resource "local_file" "key1" {
-  filename = "utc-key.pem"
+  filename = "utcK.pem"
   content  = tls_private_key.tls.private_key_pem
 }
